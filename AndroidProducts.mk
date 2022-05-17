90022772f4279ea621efca3b47130bfa51675309
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-17 17:32:12 +0200

    laurel_sprout: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: Mi A3 Script version: 2.3.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_laurel_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_laurel_sprout-user \
+    omni_laurel_sprout-userdebug \
+    omni_laurel_sprout-eng
